f6dd206b8d8ff7f02f426dbc345d6a944c4a9ae5
Toon Willems <willemstoon@gmail.com>
2010-01-15 13:18:45 +0100

    created an IRCSocket

--- watcher.rb
+++ watcher.rb
@@ -17,8 +17,9 @@ class Watcher
         files = get_files
         diff = files - old_files
         
-        # yield with all the added files (possibly empty)
-        yield diff
+        # yield with all the added files 
+        # do not yield if nothing is added
+        yield diff if diff.size > 0
        
         old_files = files
       end
@@ -34,58 +35,97 @@ class Watcher
   
 end
 
+class IRCSocket < TCPSocket
+  
+  attr_accessor :channel, :nick, :verbose
+  
+  def initialize(server,port,channel,nick,verbose=false)
+    super server,port
+    @nick,@channel = nick,channel
+    @verbose = verbose
+  end
+  
+  # override puts and gets
+  # if verbose they will print to the terminal
+  def gets
+    result = super
+    puts result if verbose
+    result
+  end
+  
+  def puts(*args)
+    # if args is nil, were not supposed to crash
+    # we make sure we don't print when it is
+    # and let super take care of the error
+    puts args.first if verbose && ! args.first.nil?
+    super
+  end
+  
+  def connect
+    self.puts "NICK #{nick}"
+    self.puts "USER #{nick.downcase} ignored ignored :#{nick}"
+    
+    setup_reply_ping_thread
+  end
+  
+  def say(text)
+    self.puts "PRIVMSG #{channel} :#{text}"
+  end
+  
+  def join
+    self.puts "JOIN #{channel}"
+  end
+  
+  def leave
+    self.puts "PART #{channel}"
+  end
+
+  def close
+    leave
+    self.puts "QUIT"
+    super
+    @pingthread.join
+  end
+
+  private
+
+  def setup_reply_ping_thread
+    @pingthread = Thread.new(self) do |socket|
+      until socket.eof?
+        if socket.gets =~ /PING :(.*)$/
+          socket.puts "PONG :#{$1}"
+        end
+      end
+    end
+  end
+  
+end
+
 class IRCFileBot
   
   def initialize(server,bot_name,channel,port,directory)
     @server,@bot_name,@channel,@port = server,bot_name,channel,port
     @watcher = Watcher.new(directory)
-
-    connect
+    @ircsocket = IRCSocket.new server,port,channel,bot_name
   end
   
   def run
     @watcher.watch do |files|
-      mainloop(files)
+      files_added(files)
       sleep 1
     end
   end
   
   private
-
-  def pipe(content)
-    puts content
-    content
-  end
   
-  def connect
-    @socket = TCPSocket.open(@server,@port)
-    @socket.puts pipe "NICK #{@bot_name}"
-    @socket.puts pipe "USER #{@bot_name.downcase} ignored ignored :#{@bot_name}"
-
-    @listenthread = Thread.new(@socket) do |socket|
-      until @socket.eof?
-        if pipe(@socket.gets) =~ /PING :(.*)$/
-          @socket.puts pipe "PONG :#{$1}"
-        end
-      end
-    end
+  def files_added(files)
+    @ircsocket.join
+    sleep 1
+    @ircsocket.say "#{files.join ", "} were added"
+    sleep 2
+    @ircsocket.leave
   end
 
-  def mainloop(files)
-    unless files.empty?
-      @socket.puts pipe "JOIN #{@channel}"
-      sleep 1
-      @socket.puts pipe "PRIVMSG #{@channel} :#{files.join ", "} were added"
-      sleep 1
-      @socket.puts pipe "PART #{@channel}"
-    end
-  end
-
-  def disconnect
-    @socket.puts pipe "QUIT"
-    @socket.close      # Probably exception due to socket.gets while closing 
-    @listenthread.join
-  end
 end
 
 # Main
