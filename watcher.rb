67956720cebfddb469ddfaba16180dccacda7cdf
David Verhasselt <david@crowdway.com>
2010-01-15 01:23:28 +0100

    Fixed indentation. Sorry ;)

--- watcher.rb
+++ watcher.rb
@@ -40,56 +40,56 @@ class IRCFileBot
     @server,@bot_name,@channel,@port = server,bot_name,channel,port
     @watcher = Watcher.new(directory)
 
-		connect
+    connect
   end
   
   def run
     @watcher.watch do |files|
       mainloop(files)
-			sleep 1
+      sleep 1
     end
   end
   
   private
 
   def pipe(content)
-  	puts content
-		content
+    puts content
+    content
   end
   
-	def connect
-		@socket = TCPSocket.open(@server,@port)
-		@socket.puts pipe "NICK #{@bot_name}"
-		@socket.puts pipe "USER #{@bot_name.downcase} ignored ignored :#{@bot_name}"
+  def connect
+    @socket = TCPSocket.open(@server,@port)
+    @socket.puts pipe "NICK #{@bot_name}"
+    @socket.puts pipe "USER #{@bot_name.downcase} ignored ignored :#{@bot_name}"
 
-		@listenthread = Thread.new(@socket) do |socket|
-			until @socket.eof?
-      	if pipe(@socket.gets) =~ /PING :(.*)$/
-        	@socket.puts pipe "PONG :#{$1}"
-	      end
-  	  end
-		end
-	end
+    @listenthread = Thread.new(@socket) do |socket|
+      until @socket.eof?
+        if pipe(@socket.gets) =~ /PING :(.*)$/
+          @socket.puts pipe "PONG :#{$1}"
+        end
+      end
+    end
+  end
 
-	def mainloop(files)
-		unless files.empty?
-			@socket.puts pipe "JOIN #{@channel}"
-			sleep 1
-			@socket.puts pipe "PRIVMSG #{@channel} :#{files.join ", "} were added"
-			sleep 1
-			@socket.puts pipe "PART #{@channel}"
-		end
-	end
+  def mainloop(files)
+    unless files.empty?
+      @socket.puts pipe "JOIN #{@channel}"
+      sleep 1
+      @socket.puts pipe "PRIVMSG #{@channel} :#{files.join ", "} were added"
+      sleep 1
+      @socket.puts pipe "PART #{@channel}"
+    end
+  end
 
-	def disconnect
-		@socket.puts pipe "QUIT"
-		@socket.close			# Probably exception due to socket.gets while closing 
-		@listenthread.join
-	end
+  def disconnect
+    @socket.puts pipe "QUIT"
+    @socket.close      # Probably exception due to socket.gets while closing 
+    @listenthread.join
+  end
 end
 
 # Main
 if __FILE__ == $0
-	bot = IRCFileBot.new "wina.ugent.be", "ZeusFileBot", "#zeus", 6666, "/tmp"
-	bot.run
+  bot = IRCFileBot.new "wina.ugent.be", "ZeusFileBot", "#zeus", 6666, "/tmp"
+  bot.run
 end
